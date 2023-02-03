d4cbdfb5308a44808accc75bdcdfe75f900dce1e
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-02-03 12:46:33 -0600

    Add files via upload

---
+++ EVA1_HOLAMUNDO/src/eva1_holamundo/EVA1_HOLAMUNDO.java
@@ -0,0 +1,13 @@
+package eva1_holamundo;
+
+public class EVA1_HOLAMUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        System.out.println("Holaa mundo >:)");
+    }
+    
+}
