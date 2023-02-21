ac60e72ea1a8d901c395f88abcc93954d1671b51
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-02-21 12:51:17 -0600

    Add files via upload

---
+++ EVA1_9_MENSAJE/src/eva1_9_mensaje/EVA1_9_MENSAJE.java
@@ -0,0 +1,17 @@
+package eva1_9_mensaje;
+
+/**
+ * @author invitado
+ */
+public class EVA1_9_MENSAJE {
+
+    public static void main(String[] args) {
+        nPrintLn("Hoola", 5);
+    }
+    public static void nPrintLn(String message, int num){
+        for (int i = 0; i < num; i++) {
+            System.out.println("message");
+        }
+    }
+    
+}
