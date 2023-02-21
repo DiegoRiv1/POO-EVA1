0dfdf1fd8a49405235c6cf8a31ea3e47d51ecf4a
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-02-21 12:43:55 -0600

    Add files via upload

---
+++ EVA1_7_METHODS/src/eva1_7_methods/EVA1_7_METHODS.java
@@ -0,0 +1,14 @@
+package eva1_7_methods;
+
+/**
+ * @author Diego Rivera
+ */
+public class EVA1_7_METHODS {
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+    public static void myMethod(){
+            System.out.println("Hola Hola amigo");
+    }
+}
