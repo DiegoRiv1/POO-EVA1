8a1844506af4aa92d8978e810f7d375770d5b7e1
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-02-23 12:47:50 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,22 @@
+package eva1_15_sobrecarga;
+
+/**
+ * @author Diego Rivera
+ */
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hellooo World");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    public static void Algo(){
+
+    }
+    public static void Algo(int x){
+
+    }
+}
+
