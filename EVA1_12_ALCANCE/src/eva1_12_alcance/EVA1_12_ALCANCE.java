641ea0103b0ccfb6c269e956f6f6af7b00fc2fcf
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-02-23 12:46:57 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,15 @@
+package eva1_12_alcance;
+
+/**
+ * @author Diego Rivera
+ */
+public class EVA1_12_ALCANCE {
+
+    public static void main(String[] args) {
+        //System.out.println(x); Marca error ya que la x aun no es creada.
+
+	int x = 100; //Se declara x.
+	System.out.println(x); //Aqui ya existe x.
+    }
+    
+}
