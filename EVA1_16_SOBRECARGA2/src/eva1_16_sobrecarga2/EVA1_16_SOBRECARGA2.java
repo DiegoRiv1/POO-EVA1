727672f22d3a6ccb88034edcb4096c694e2eb5bf
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-02-24 12:49:44 -0600

    Add files via upload

---
+++ EVA1_16_SOBRECARGA2/src/eva1_16_sobrecarga2/EVA1_16_SOBRECARGA2.java
@@ -0,0 +1,31 @@
+package eva1_16_sobrecarga2;
+
+/**
+ * @author Diego
+ */
+public class EVA1_16_SOBRECARGA2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Suma: "+ suma('A', 9));
+        System.out.println("Suma: "+ suma(4.32442, 9.35332));
+        System.out.println("Suma: "+ suma("Mundo ", "Hola"));
+        suma();
+    }
+            //Suma (int, int)
+    public static int suma(int val1, int val2){
+        return val1+val2;
+    }
+            //Suma(double, double)
+    public static double suma(double val1, double val2){
+        return val1+val2;
+    }
+            //Suma(String, String)
+    public static String suma(String val1, String val2){
+        return val1+val2;
+    }
+                //Suma(String, String)
+    public static void suma(){
+        System.out.println("Suma: sin argumentos");
+    }
+}
